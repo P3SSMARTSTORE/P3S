1f43d2f208d54486cd0c46b510526218f35094a5
P3SSMARTSTORE <kratika3888@gmail.com>
2026-08-04 16:12:06 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -111,3 +111,15 @@ if (slides.length > 0) {
     }, 5000);
 
 }
+const slides = document.querySelectorAll(".slide");
+let currentSlide = 0;
+
+if (slides.length > 1) {
+    setInterval(function () {
+        slides[currentSlide].classList.remove("active");
+
+        currentSlide = (currentSlide + 1) % slides.length;
+
+        slides[currentSlide].classList.add("active");
+    }, 5000);
+}
